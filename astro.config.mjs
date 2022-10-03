8979d3c8a327d2dfbd537350931cb8f340cd1ac5
himyjan <51815522+himyjan@users.noreply.github.com>
2022-10-03 19:32:35 +0800

    update package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
 import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
-import vercel from '@astrojs/vercel/serverless';
+import solid from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  // Enable React to support React JSX components.
-  integrations: [react(), vue(), tailwind(), vue()]
+	// Enable many frameworks to support all different kinds of components.
+	integrations: [preact(), react(), svelte(), vue(), solid()],
 });
